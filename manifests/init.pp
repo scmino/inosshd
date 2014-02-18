59b82687876e7b7a06d74276b5202947a28db240
Olivier Fournier <olivier.fournier@swisscom.com>
2014-02-18 10:04:53 +0100

    Add support for RedHat derivatives

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
   *       configuration customization needs to be performed)
   *
   *  include inosshd
-  * 
+  *
   *  Ensure OpenSSH daemon is present and running and set default settings
   *    => Typically directly called by the inospec module for zones where some
   *       configuration customization needs to be performed
@@ -64,29 +64,40 @@ class inosshd {
 
   define build_config($default_settings, $custom_settings) {
 
-    # Ensure OpenSSH is enabled and running
-    service { 'ssh':
-      ensure => running,
-      enable => true,
+    # Define groups depending on the OS
+    $sshsrv = $::osfamily ? {
+      RedHat  => 'sshd',
+      Debian  => 'ssh',
+      default => undef,
     }
 
-    # Build configuration array for augeas
-    if $default_settings == undef and $custom_settings == undef {
-      $_settings = ''
-    } elsif $default_settings == undef {
-      $_settings = $custom_settings
-    } elsif $custom_settings == undef {
-      $_settings = $default_settings
-    } else {
-      $_settings = concat( $default_settings, $custom_settings )
-    }
+    if $sshsrv {
 
-    # Use augeas to apply modifications
-    augeas { 'sshd_config':
-      context => '/files/etc/ssh/sshd_config',
-      changes => $_settings,
-      notify  => Service['ssh'],
-    }
+      # Ensure OpenSSH is enabled and running
+      service { $sshsrv:
+        ensure => running,
+        enable => true,
+      }
+
+      # Build configuration array for augeas
+      if $default_settings == undef and $custom_settings == undef {
+        $_settings = ''
+      } elsif $default_settings == undef {
+        $_settings = $custom_settings
+      } elsif $custom_settings == undef {
+        $_settings = $default_settings
+      } else {
+        $_settings = concat( $default_settings, $custom_settings )
+      }
+
+      # Use augeas to apply modifications
+      augeas { 'sshd_config':
+        context => '/files/etc/ssh/sshd_config',
+        changes => $_settings,
+        notify  => Service[$sshsrv],
+      }
+
+    } else { alert('Unsupported OS') }
 
   }
 
